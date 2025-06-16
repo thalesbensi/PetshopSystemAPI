eafaed91023d6e10eb5d9de6e0880fba41f4dbe6
thalesbensi <thalesbensi@outlook.com>
2025-06-15 21:07:23 -0300

    feat: Implement Tutor entity

---
+++ PetshopAPISystem/Domain/Models/Tutor.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetshopAPISystem.Domain.Models;
+
+public class Tutor
+{
+    public long Id { get; set; }
+    
+    [Required]
+    public string Name { get; set; }
+    
+    [EmailAddress]
+    public string Email { get; set; }
+    
+    public List<Pet> Pets { get; set; } = new List<Pet>();
+}
