46f7ca3b9447633f06b79dd9e1232cc71742b178
thalesbensi <thalesbensi@outlook.com>
2025-06-15 21:06:43 -0300

    feat: Implement Pet entity

---
+++ PetshopAPISystem/Domain/Models/Pet.cs
@@ -0,0 +1,12 @@
+﻿namespace PetshopAPISystem.Domain.Models;
+
+public class Pet
+{
+    public long Id;
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public PetType Type { get; set; }
+    public string Race { get; set; }
+    public long TutorId { get; set; }
+    public Tutor Tutor { get; set; }
+}
