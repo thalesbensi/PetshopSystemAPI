dc4fe9d2d3beaf6acc1640822d90906d1e02decc
thalesbensi <thalesbensi@outlook.com>
2025-06-16 13:39:34 -0300

    refac: Do little correction on Id getter and setter

--- PetshopAPISystem/Domain/Models/Pet.cs
+++ PetshopAPISystem/Domain/Models/Pet.cs
@@ -2,7 +2,7 @@
 
 public class Pet
 {
-    public long Id;
+    public long Id { get; set; }
     public string Name { get; set; }
     public int Age { get; set; }
     public PetType Type { get; set; }
