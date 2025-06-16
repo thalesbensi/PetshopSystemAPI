e89fbd45b67683f3899e28edb1dd60356ca89080
thalesbensi <thalesbensi@outlook.com>
2025-06-15 21:07:43 -0300

    feat: Implement DbContext

---
+++ PetshopAPISystem/Domain/Contexts/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetshopAPISystem.Domain.Models;
+
+namespace PetshopAPISystem.Domain.Contexts;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Tutor> Tutors { get; set; }
+    public DbSet<Pet> Pets { get; set; }
+    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+}
