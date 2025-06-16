d5f3e304753a6a48c45ab2df11cc8cc9df6135e8
thalesbensi <thalesbensi@outlook.com>
2025-06-15 21:08:17 -0300

    feat: Setup application to use InMemoryDatabase

--- PetshopAPISystem/Program.cs
+++ PetshopAPISystem/Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using PetshopAPISystem.Domain.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseInMemoryDatabase("PetshopDb"));
 
 var app = builder.Build();
 
