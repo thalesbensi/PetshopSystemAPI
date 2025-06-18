3933859f8375129fecb1c24efaf028c60503dc4c
thalesbensi <thalesbensi@outlook.com>
2025-06-18 14:53:42 -0300

    rafac: Update Tutor entity properties

--- PetshopAPISystem/Domain/Models/Tutor.cs
+++ PetshopAPISystem/Domain/Models/Tutor.cs
@@ -10,5 +10,12 @@ public class Tutor
     public string Name { get; set; }
     
     [EmailAddress]
+    [Required]
     public string Email { get; set; }
+    
+    [Required]
+    public string Password { get; set; }
+    
+    [Required]
+    public Role[] Roles { get; set; }
 }
