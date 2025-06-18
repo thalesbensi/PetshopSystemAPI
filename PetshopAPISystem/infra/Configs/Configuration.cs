61d0d9010e3558d37aa2fe7c5bdcebe953753800
thalesbensi <thalesbensi@outlook.com>
2025-06-18 18:41:36 -0300

    feat: Add PrivateKey

--- PetshopAPISystem/infra/Configs/Configuration.cs
+++ PetshopAPISystem/infra/Configs/Configuration.cs
@@ -2,5 +2,5 @@
 
 public class Configuration
 {
-    public static string PrivateKey { get; set; } = "rB)^}4EFodUoa6>N^ALC";  //Should not be hardcoded, its supposed to be a environment variable or something like that but as a test, gonna be like that by now
+    public static string PrivateKey { get; set; } = "MySuperSecretPrivateKey1234567890!@#";  //Should not be hardcoded, its supposed to be a environment variable or something like that but as a test, gonna be like that by now
 }
