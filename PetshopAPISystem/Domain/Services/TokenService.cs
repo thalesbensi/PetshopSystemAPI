edfb9c49c889ee7d5e8aac24d6de5d21e9fb2a78
thalesbensi <thalesbensi@outlook.com>
2025-06-18 15:54:16 -0300

    feat: Implement Token Service to generate tokens

---
+++ PetshopAPISystem/Domain/Services/TokenService.cs
@@ -0,0 +1,45 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using PetshopAPISystem.Domain.Contexts;
+using PetshopAPISystem.Domain.Models;
+using PetshopAPISystem.infra.Configs;
+
+namespace PetshopAPISystem.Domain.Services;
+
+public class TokenService
+{
+    public static string KEY = Configuration.PrivateKey;
+    
+    public string GenerateToken(Tutor tutor)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        
+        var key = Encoding.ASCII.GetBytes(KEY);
+
+        var credentials = new SigningCredentials(
+            new SymmetricSecurityKey(key), 
+            SecurityAlgorithms.HmacSha256);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = GenerateClaimsIdentity(tutor),
+            SigningCredentials = credentials,
+            Expires = DateTime.UtcNow.AddHours(2),
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        
+        return tokenHandler.WriteToken(token);
+    }
+
+    public static ClaimsIdentity GenerateClaimsIdentity(Tutor tutor)
+    {
+        var claimsIdentity = new ClaimsIdentity();
+        claimsIdentity.AddClaim(new Claim(ClaimTypes.Name, tutor.Email));
+        foreach (var role in tutor.Roles)
+            claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, role.ToString()));
+        return claimsIdentity;
+    }
+}
---
+++ PetshopAPISystem/infra/Configs/Configuration.cs
@@ -0,0 +1,6 @@
+﻿namespace PetshopAPISystem.infra.Configs;
+
+public class Configuration
+{
+    public static string PrivateKey { get; set; } = "rB)^}4EFodUoa6>N^ALC";  //Should not be hardcoded, its supposed to be a environment variable or something like that but as a test, gonna be like that by now
+}
