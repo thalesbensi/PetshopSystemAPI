db77c12f938817651dbb28eb0e4a1ecf81ccc80d
thalesbensi <thalesbensi@outlook.com>
2025-06-18 18:43:56 -0300

    feat: Enable Authentication

--- PetshopAPISystem/Program.cs
+++ PetshopAPISystem/Program.cs
@@ -1,6 +1,10 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using PetshopAPISystem.Domain.Contexts;
 using PetshopAPISystem.Domain.Services;
+using PetshopAPISystem.infra.Configs;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,9 +16,26 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 builder.Services.AddScoped<PetService>();
 builder.Services.AddScoped<TutorService>();
+builder.Services.AddScoped<TokenService>();
+builder.Services.AddScoped<AuthService>();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseInMemoryDatabase("PetshopDb"));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        var key = Encoding.ASCII.GetBytes(Configuration.PrivateKey);
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key)
+        };
+    });
+
 
 var app = builder.Build();
 
@@ -23,8 +44,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseHttpsRedirection();
-
 app.MapControllers();
 app.Run();
