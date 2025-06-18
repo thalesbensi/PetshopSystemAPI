9be033813708712f24edb8ac91c9ea31d83deb50
thalesbensi <thalesbensi@outlook.com>
2025-06-18 18:42:50 -0300

    feat: Implement AuthController

---
+++ PetshopAPISystem/Api/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetshopAPISystem.Api.DTOs.Requests;
+using PetshopAPISystem.Domain.Models;
+using PetshopAPISystem.Domain.Services;
+
+[ApiController]
+[Route("api/auth")]
+public class AuthController : ControllerBase
+{
+
+    public readonly AuthService AuthService;
+
+    public AuthController(AuthService authService)
+    {
+        AuthService = authService;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register( Tutor tutor)
+    {
+        var newTutor = await AuthService.Register(tutor);
+        if (newTutor == null) return Conflict("Email Already Registered");
+        return Ok("Registration Successful");
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(TutorLoginRequestDTO loginRequest)
+    {
+        var token = await AuthService.Login(loginRequest);
+        if (token == null) return Unauthorized("Something went wrong while logging in, please verify email and password");
+        return Ok(token);
+    }
+}
